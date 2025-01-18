8e3e650001cde7ce279f240ad1a45c8d7e911d71
M Reza Kouchaki <93693562+mrkouchaki@users.noreply.github.com>
2025-01-17 21:51:51 -0600

    Update ml4.py

--- ml4.py
+++ ml4.py
@@ -68,21 +68,6 @@ class RNN_Autoencoder(nn.Module):
         return x_reconstructed
 
 
-
-        # def forward(self, x):
-        #     _, (h, _) = self.encoder_rnn(x)
-        #     print(f"Shape of encoder hidden state h[-1]: {h[-1].shape}")
-        #     latent = self.hidden_to_latent(h[-1])
-        #     print(f"Shape of latent: {latent.shape}")
-        #     h_decoded = self.latent_to_hidden(latent).unsqueeze(0)
-        #     print(f"Shape of decoded hidden state: {h_decoded.shape}")
-        #     x_transformed = self.input_to_hidden(x)
-        #     print(f"Shape of transformed input: {x_transformed.shape}")
-        #     x_reconstructed, _ = self.decoder_rnn(x_transformed, (h_decoded, torch.zeros_like(h_decoded)))
-        #     print(f"Shape of reconstructed output: {x_reconstructed.shape}")
-        #     #x_reconstructed, _ = self.decoder_rnn(x, (h_decoded, torch.zeros_like(h_decoded)))
-        #     return x_reconstructed
-
     # Initialize model, loss, and optimizer
     n_features = 3  # Adjust based on the number of features (e.g., tx_pkts, tx_error, cqi)
     model = RNN_Autoencoder(input_dim=n_features, hidden_dim=hidden_dim, latent_dim=latent_dim)
